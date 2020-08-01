a90cef40461094c5dfb8c77a131881e6a96e16b4
Emil <emil.j.carlen@gmail.com>
2020-08-01 11:13:35 +0200

    Add config stuff

--- src/main/kotlin/me/loidsemus/crowdfunding/config/MainConfig.kt
+++ src/main/kotlin/me/loidsemus/crowdfunding/config/MainConfig.kt
@@ -1,6 +1,28 @@
 package me.loidsemus.crowdfunding.config
 
+import de.exlll.configlib.annotation.ConfigurationElement
+import de.exlll.configlib.annotation.ElementType
 import de.exlll.configlib.configs.yaml.YamlConfiguration
 import java.io.File
 
-class MainConfig(dir: File) : YamlConfiguration(File(dir, "config.yml").toPath(), YamlProperties.DEFAULT)
+class MainConfig(dir: File) : YamlConfiguration(File(dir, "config.yml").toPath(), YamlProperties.DEFAULT) {
+
+    var actions = Actions()
+
+}
+
+@ConfigurationElement
+class Actions(
+    @ElementType(Command::class)
+    var commands: MutableList<Command> = mutableListOf(
+        Command(),
+        Command("time set night", "Sets the time to night for all players", 2500.0)
+    )
+)
+
+@ConfigurationElement
+class Command(
+    var command: String = "time set day",
+    var description: String = "Sets the time to day for all players",
+    var minimumCost: Double = 5000.0
+)
